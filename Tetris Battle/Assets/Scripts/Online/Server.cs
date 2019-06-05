7b4398d8e72cc6b2c7ba3e45888c85936d48a412
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2019-06-05 00:54:16 -0300

    Initial commit

---
+++ Tetris Battle/Assets/Scripts/Online/Server.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+
+public class Server : MonoBehaviourPunCallbacks
+{
+    int maxPersons;
+    public Server (int persons ) {
+        maxPersons = persons;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tetris Battle/Assets/Scripts/Online/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartMenu : MonoBehaviour
+{
+    TMP_InputField players;
+    TMP_InputField ip;
+
+    public void EnterToServer() {
+        //Crear un nuevo server yluego pasar a la escena del juego, usando la cantidad de players que esta ahi
+    }
+    public void EnterToGame() {
+        //Entra al juego usand la ip que esta ahi arriba
+    }
+}
