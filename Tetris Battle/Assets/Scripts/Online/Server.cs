e9c6fa27cd69c9998564c18b3720da85482e6099
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2019-06-19 16:37:12 -0300

    Lloro, intento pero nada

--- Tetris Battle/Assets/Scripts/Online/Server.cs
+++ Tetris Battle/Assets/Scripts/Online/Server.cs
@@ -3,23 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class Server : MonoBehaviourPunCallbacks
-{
-    int maxPersons;
-    public Server (int persons ) {
-        maxPersons = persons;
+public class Server : MonoBehaviourPun {
+    public static Server Instance { get; private set; } //El private es para que no te hackeen y que lo seteen(xd) de otro lado 
+    public Dictionary<Player, Person> persons = new Dictionary<Player, Person>();
+    PhotonView _view;
+    public Player server;
+    //Cuando cambia escena ahi es cuando crea los persons?
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _view = GetComponent<PhotonView>();
+        if ( !Instance ) {
+            if ( _view.IsMine )
+                _view.RPC("SetReferenceToSelf", RpcTarget.AllBuffered, PhotonNetwork.LocalPlayer);  //Buffered: todos los que se conecten tarde reciben este mensaje
+
+        } else
+            PhotonNetwork.Destroy(gameObject);  //Me destrushe
+
+
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    [PunRPC]    //Funcion que se llama enel servidory se replica en todos los clientes 
+    public void SetReferenceToSelf( Player p ) {    //Esto es como un singleton pero para redes
+        Instance = this;
+        server = p;
         
+        if ( !PhotonNetwork.IsMasterClient )    //Wachin, sos el master?
+            _view.RPC("AddPlayer", server, PhotonNetwork.LocalPlayer);//Flasheaste, te agrego un player
+            
+    }
+
+    [PunRPC]
+    public void AddPlayer( Player p ) {
+        if ( !_view.IsMine )
+            return;     //Wat is dis?
+        var newPlayer = PhotonNetwork.Instantiate("Person",
+                        new Vector3(Random.Range(0, 3),
+                        Random.Range(0, 3),
+                        Random.Range(0, 3)),
+                        Quaternion.identity).GetComponent<Person>();
+        persons.Add(p, newPlayer);
+        foreach ( var item in persons ) {
+            Debug.Log(item);
+        }
+    }
+
+    [PunRPC] 
+    public void StartGame(int value) {
+        PhotonNetwork.LoadLevel(value + 1);
+        PhotonNetwork.AutomaticallySyncScene = true;    //For some reason,this only happends in the last 
+
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {/* maybe this goes here?
+        if ( PhotonNetwork.PlayerList.Length == 3 )
+            PhotonNetwork.LoadLevel(1);
+        if ( PhotonNetwork.PlayerList.Length == 4 )
+            PhotonNetwork.LoadLevel(2);
+        if ( PhotonNetwork.PlayerList.Length == 5 )
+            PhotonNetwork.LoadLevel(3);
+            */
     }
 }
+/*    [PunRPC]
+    void ChangeScene() {
+        if ( PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers ) {
+            Debug.Log("CHANGING SCENE REEEEEEE");
+            PhotonNetwork.AutomaticallySyncScene = false;    //For some reason,this only happends in the last 
+            PhotonNetwork.LoadLevel(dropdown.value + 1);
+        }
+
+    }
+*/
--- Tetris Battle/Assets/Scripts/Online/StartMenu.cs
+++ Tetris Battle/Assets/Scripts/Online/StartMenu.cs
@@ -7,21 +7,26 @@ using TMPro;
 
 public class StartMenu : MonoBehaviourPunCallbacks {
     public TMP_Text conectionState;
-
+    public TMP_Dropdown dropdown;
     bool isHost;
+    int playerCount;
+
+    private void Awake() {
+        DontDestroyOnLoad(this);
+    }
 
     private void Start() {
         DontDestroyOnLoad(this);
     }
-
     public void HostServer() {
         isHost = true;
         PhotonNetwork.ConnectUsingSettings();
         conectionState.gameObject.SetActive(true);
-
     }
 
     public void Disconnect() {
+        playerCount--;
+        Debug.Log("disconectto" + playerCount);
         PhotonNetwork.Disconnect();
     }
 
@@ -33,7 +38,8 @@ public class StartMenu : MonoBehaviourPunCallbacks {
 
     }
     public override void OnConnectedToMaster() {
-        conectionState.text = "CONECTANDO AL SERVER";
+        playerCount++;
+        conectionState.text = "CONECTADO AL SERVER";
         PhotonNetwork.JoinLobby(TypedLobby.Default);
     }
 
@@ -44,24 +50,40 @@ public class StartMenu : MonoBehaviourPunCallbacks {
     public override void OnJoinedLobby() {
         conectionState.text = "EN EL LOBBY";
 
+        byte b = 0;
+        if ( dropdown.value == 0 ) b = 3;
+        else if ( dropdown.value == 1 ) b = 4;
+        else if (dropdown.value == 2 ) b = 5;
+
+        
         if ( isHost ) {
             //Esta es la instancia del juego, en terminos de network no de escenas 
-            PhotonNetwork.CreateRoom("MainRoom", new RoomOptions() { MaxPlayers = 5 });
-            return;
+            PhotonNetwork.CreateRoom("MainRoom", new RoomOptions() { MaxPlayers = b });
         }
+
+        //recien cuando hay gente los tiras a la room?
+
+
         PhotonNetwork.JoinRandomRoom();
+
     }
 
     public override void OnJoinedRoom() {
         conectionState.text = "EN LA HABITACION";
-        //PhotonNetwork.LoadLevel(1);
-        /*
+
+        Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount + " ndeah");
+
+        ChangeScene();      //Right?
+        //only the master client may load the level
+
         if ( isHost )
-            PhotonNetwork.Instantiate("ServerNetwork", Vector3.zero, Quaternion.identity);
+            PhotonNetwork.Instantiate("Server7w7", Vector3.zero, Quaternion.identity);
+        /*
         else
-            PhotonNetwork.Instantiate("Controller", Vector3.zero, Quaternion.identity);
+            PhotonNetwork.Instantiate("Person", Vector3.zero, Quaternion.identity);
         */
     }
+    //public override void update
 
     public override void OnJoinRandomFailed( short returnCode, string message ) {
         conectionState.text = "FALLO POR QUE " + message;
@@ -70,6 +92,15 @@ public class StartMenu : MonoBehaviourPunCallbacks {
 
     public override void OnCreatedRoom() {
         conectionState.text = "CREO UNA HABITACION";
+    }
+
+    [PunRPC]
+    void ChangeScene() {
+        if ( PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers ) {
+            Debug.Log("CHANGING SCENE REEEEEEE");
+            Server.Instance.StartGame(dropdown.value + 1);
+        }
+
     }
 
 
